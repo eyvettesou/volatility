f17c9c09eba6e60745ec5534cf4cf3dd5745fdb1
Eyvette Sou <eyvette.sou@procore.com>
2017-10-29 21:23:50 -0700

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Menu, Icon } from 'antd';
-const SubMenu = Menu.SubMenu;
-const MenuItemGroup = Menu.ItemGroup;
+import { Menu } from 'antd';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
 class App extends Component {
   state = {
-    current: 'mail',
+    current: 'home',
   }
+
   handleClick = (e) => {
-    console.log('click ', e);
     this.setState({
       current: e.key,
     });
+  }
+
+  home = () => {
+    return(
+      <div>
+        <h2>Hello</h2>
+      </div>
+    )
+  }
+
+  about = () => {
+    return(
+      <div>
+        <h2>ABOUT</h2>
+      </div>
+    )
+  }
+
+  artists = () => {
+    return(
+      <div>
+        <h2>ARTISTS</h2>
+      </div>
+    )
+  }
+
+  photos = () => {
+    return(
+      <div>
+        <h2>Photos</h2>
+      </div>
+    )
+  }
+
+  videos = () => {
+    return(
+      <div>
+        <h2>Video</h2>
+      </div>
+    )
   }
 
   render() {
@@ -21,29 +64,40 @@ class App extends Component {
         <div className="App-logo">
           logo goes here
         </div>
-        <Menu
-           onClick={this.handleClick}
-           selectedKeys={[this.state.current]}
-           mode="horizontal"
-           className="App-menu"
-         >
-           <Menu.Item key="mail" className="App-menu-items">
-             HOME
-           </Menu.Item>
-           <Menu.Item key="mail" className="App-menu-items">
-             ABOUT
-           </Menu.Item>
-           <Menu.Item key="mail" className="App-menu-items">
-             ARTISTS
-           </Menu.Item>
-           <Menu.Item key="mail" className="App-menu-items">
-             PHOTOS
-           </Menu.Item>
-           <Menu.Item key="mail" className="App-menu-items">
-             VIDEOS
-           </Menu.Item>
-         </Menu>
-        <div className="App-photostream"> goes here </div>
+        <Router>
+          <div>
+            <Menu
+              onClick={this.handleClick}
+              selectedKeys={[this.state.current]}
+              mode="horizontal"
+              className="App-menu"
+            >
+              <Menu.Item key="home" className="App-menu-items">
+                <Link to="/">HOME</Link>
+              </Menu.Item>
+              <Menu.Item key="about" className="App-menu-items">
+                <Link to="/about">ABOUT</Link>
+              </Menu.Item>
+              <Menu.Item key="artists" className="App-menu-items">
+                <Link to="/artists">ARTISTS</Link>
+              </Menu.Item>
+              <Menu.Item key="photos" className="App-menu-items">
+                <Link to="/photos">PHOTOS</Link>
+              </Menu.Item>
+              <Menu.Item key="videos" className="App-menu-items">
+                <Link to="/videos">VIDEOS</Link>
+              </Menu.Item>
+            </Menu>
+            <div className="App-photostream">
+              <Route exact path="/" component={this.home}/>
+              <Route path="/about" component={this.about}/>
+              <Route path="/artists" component={this.artists}/>
+              <Route path="/photos" component={this.photos}/>
+              <Route path="/videos" component={this.videos}/>
+
+            </div>
+          </div>
+        </Router>
         <div className="App-footer">
           Contact us
         </div>
